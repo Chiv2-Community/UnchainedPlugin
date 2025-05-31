8ffc2ef330807b64181888a5d03cb15bfaf021f9
Jacob <jacoby6000@gmail.com>
2025-05-31 17:16:24 -0500

    Migrate etcHooks.h

--- src/legacy_hooks/sigs.h
+++ src/legacy_hooks/sigs.h
@@ -4,7 +4,6 @@
 
 #define FUNCTYPES \
 	etype(UTBLLocalPlayer_Exec) \
-	etype(GetGameInfo) \
 	etype(ApproveLogin) \
 	etype(MaxFuncType) //this always needs to be last
 
@@ -21,8 +20,6 @@ static const char* signatures[F_MaxFuncType + 1] =
 {
 	/*"UTBLLocalPlayer::Exec"*/
 	"75 18 ?? ?? ?? ?? 75 12 4d 85 f6 74 0d 41 38 be ?? ?? ?? ?? 74 04 32 db eb 9b 48 8b 5d 7f 49 8b d5 4c 8b 45 77 4c 8b cb 49 8b cf",
-	/*"GetGameInfo"*/
-	"48 8B C4 48 89 58 ?? 48 89 50 ?? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ?? ?? ?? ?? 48 81 EC E0 02 00 00",
 	/*ApproveLogin*/
 	"48 89 5C 24 18 48 89 74 24 20 55 57 41 54 41 55 41 56 48 8D 6C 24 C9 48 81 EC A0 00 00 00 8B",
 	/*MaxFuncType*/
