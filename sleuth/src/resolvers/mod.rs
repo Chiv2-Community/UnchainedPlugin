f4be773f7ea20908cce35a965bd58f9593f3925d
Knutschbert <knutschbert@gmail.com>
2025-06-05 02:02:33 +0200

    define_pattern_resolver will accept both lists and platform dictionaries

--- sleuth/src/resolvers/mod.rs
+++ sleuth/src/resolvers/mod.rs
@@ -1,6 +1,44 @@
 #[macro_use]
 mod macros;
 
+use once_cell::sync::OnceCell;
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum PlatformType {
+    EGS,
+    STEAM,
+    XBOX,
+    OTHER,
+}
+
+pub static PLATFORM: OnceCell<PlatformType> = OnceCell::new();
+
+pub fn current_platform() -> PlatformType {
+    *PLATFORM.get().expect("Platform not initialized")
+}
+
+impl std::fmt::Display for PlatformType {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::str::FromStr for PlatformType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_uppercase().as_str() {
+            "EGS" => Ok(PlatformType::EGS),
+            "STEAM" => Ok(PlatformType::STEAM),
+            "XBOX" => Ok(PlatformType::XBOX),
+            "OTHER" => Ok(PlatformType::OTHER),
+            _ => Err(()),
+        }
+    }
+}
+
 pub mod admin_control;
 pub mod asset_loading;
 pub mod backend_hooks;
--- sleuth/src/resolvers/unchained_integration.rs
+++ sleuth/src/resolvers/unchained_integration.rs
@@ -5,10 +5,10 @@
 // define_pattern_resolver!(LoadFrontEndMap, [
 // ]);
 
-define_pattern_resolver!(InternalGetNetMode, [
-    "40 53 48 81 EC 90 00 00 00 48 8B D9 48 8B 49 38 48 85 C9", // EGS
-    "40 57 48 81 EC 90 00 00 00 48 8B F9 48 8B", // STEAM
-]);
+define_pattern_resolver!(InternalGetNetMode, {
+    EGS: ["40 53 48 81 EC 90 00 00 00 48 8B D9 48 8B 49 38 48 85 C9"], // EGS
+    STEAM: ["40 57 48 81 EC 90 00 00 00 48 8B F9 48 8B"], // STEAM
+});
 
 define_pattern_resolver!(UNetDriver_GetNetMode, [
     "48 83 EC 28 48 8B 01 ?? ?? ?? ?? ?? ?? 84 C0 ?? ?? 33 C0 38 ?? ?? ?? ?? 02 0F 95 C0 FF C0 48 83 C4",
